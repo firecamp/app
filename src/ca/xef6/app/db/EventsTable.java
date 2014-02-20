3ecb3996008b2031a3beb547caf382adafdd2bf6
FS <u1m6xq2r@gmail.com>
2014-02-20 12:50:24 -0500

    Still working on database

--- src/ca/xef6/app/db/EventsTable.java
+++ src/ca/xef6/app/db/EventsTable.java
@@ -1,5 +1,23 @@
 package ca.xef6.app.db;
 
+import android.database.sqlite.SQLiteDatabase;
+
 class EventsTable {
+
+    private static final TableColumn   COLUMN_ID   = new TableColumn("_id", "integer primary key autoincrement");
+    private static final TableColumn   COLUMN_NAME = new TableColumn("name", "text not null");
+
+    private static final TableColumn[] COLUMNS     = {
+                                                   COLUMN_ID,
+                                                   COLUMN_NAME,
+                                                   };
+
+    public static final String         NAME        = "events";
+
+    public static void onCreate(SQLiteDatabase db) {
+    }
+
+    public static void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
 
 }
---
+++ src/ca/xef6/app/db/TableColumn.java
@@ -0,0 +1,13 @@
+package ca.xef6.app.db;
+
+final class TableColumn {
+
+    public final String name;
+    public final String type;
+
+    public TableColumn(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+}
