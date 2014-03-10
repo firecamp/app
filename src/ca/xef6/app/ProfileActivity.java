d8c8d39d1c56e806a460cab93672c59bb97c6d82
FS <aaa@bbb.ccc>
2014-03-10 14:22:52 -0400

    Used contact_profile

--- src/ca/xef6/app/ProfileActivity.java
+++ src/ca/xef6/app/ProfileActivity.java
@@ -8,7 +8,7 @@ public class ProfileActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.profile);
+		setContentView(R.layout.contact_profile);
 	}
 
 }
