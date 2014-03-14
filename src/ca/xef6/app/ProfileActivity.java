92f4213c26d67c10b51fc98499ce35208054e49f
FS <aaa@bbb.ccc>
2014-03-14 14:28:42 -0400

    Starting FB messenger on click

--- src/ca/xef6/app/ProfileActivity.java
+++ src/ca/xef6/app/ProfileActivity.java
@@ -1,8 +1,10 @@
 package ca.xef6.app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import ca.xef6.app.ui.Activity;
@@ -13,6 +15,7 @@ public class ProfileActivity extends Activity {
 
 	ImageButton profilePictureButton;
 	TextView userNameView;
+	String userId;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +32,14 @@ public class ProfileActivity extends Activity {
 			}
 		}
 		Log.i("ProfileActivity", "GOT USER id = " + user.getId());
+		userId = user.getId();
 		new ProfilePictureDownloadTask(profilePictureButton, getResources()).execute(user.getId());
 		userNameView.setText(user.getName());
+	}
+
+	public void startChat(View v) {
+		Uri uri = Uri.parse("fb://messaging/" + userId);
+		startActivity(new Intent(Intent.ACTION_VIEW, uri));
 	}
 
 }
