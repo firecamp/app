413ee211284a73b8a8bbcc92710d280c4ebc80ea
FS <aaa@bbb.ccc>
2014-03-18 14:55:47 -0400

    Fixed image duplication bug

--- src/ca/xef6/app/EventsFragment.java
+++ src/ca/xef6/app/EventsFragment.java
@@ -19,59 +19,64 @@ import ca.xef6.app.util.EventAdapter;
 
 public class EventsFragment extends ListFragment implements LoaderCallbacks<Cursor> {
 
-    private EventAdapter adapter;
+	private EventAdapter adapter;
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        setHasOptionsMenu(true);
-        adapter = new EventAdapter(getActivity(), R.layout.row_events, null, 0);
-        setListAdapter(adapter);
-        getLoaderManager().initLoader(0, null, this);
-    }
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		setHasOptionsMenu(true);
+		adapter = new EventAdapter(getActivity(), R.layout.row_events, null, 0);
+		setListAdapter(adapter);
+		reload();
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
-        return new CursorLoader(getActivity(), ContentProvider.CONTENT_URI, EventsTable.ALL_COLUMNS, null, null, null);
-    }
+	@Override
+	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+		return new CursorLoader(getActivity(), ContentProvider.CONTENT_URI, EventsTable.ALL_COLUMNS, null, null, null);
+	}
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.events, menu);
-    }
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.events, menu);
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.events, container, false);
-        return view;
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.events, container, false);
+		return view;
+	}
 
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        adapter.swapCursor(null);
-    }
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		adapter.swapCursor(null);
+	}
 
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        adapter.swapCursor(data);
-    }
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		adapter.swapCursor(data);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case R.id.action_create:
-            startActivity(new Intent(getActivity(), CreateEventActivity.class));
-            break;
-        default:
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_create:
+			startActivity(new Intent(getActivity(), CreateEventActivity.class));
+			reload();
+			break;
+		default:
+			return false;
+		}
+		return true;
+	}
+
+	private void reload() {
+		getLoaderManager().initLoader(0, null, this);
+	}
 
 }
--- src/ca/xef6/app/util/EventAdapter.java
+++ src/ca/xef6/app/util/EventAdapter.java
@@ -12,35 +12,38 @@ import ca.xef6.app.db.EventsTable;
 
 public class EventAdapter extends ResourceCursorAdapter {
 
-    public EventAdapter(Context context, int layout, Cursor c, int flags) {
-        super(context, layout, c, flags);
-    }
+	public EventAdapter(Context context, int layout, Cursor c, int flags) {
+		super(context, layout, c, flags);
+	}
 
-    @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+	@Override
+	public void bindView(View view, Context context, Cursor cursor) {
 
-        String imageUrl = cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_IMAGEURL));
+		String imageUrl = cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_IMAGEURL));
+		//Log.w("EventAdapter", "imageUrl = " + imageUrl);
+		ImageView iv = ((ImageView) view.findViewById(R.id.event_row_image));
+		if (imageUrl != null) {
+			iv.setImageBitmap(BitmapFactory.decodeFile(imageUrl));
+		} else {
+			iv.setVisibility(View.GONE);
+		}
 
-        if (imageUrl != null) {
-            ((ImageView) view.findViewById(R.id.event_row_image)).setImageBitmap(BitmapFactory.decodeFile(imageUrl));
-        }
+		((TextView) view.findViewById(R.id.event_row_name)).setText(
+				cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_NAME)));
 
-        ((TextView) view.findViewById(R.id.event_row_name)).setText(
-                cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_NAME)));
+		((TextView) view.findViewById(R.id.event_row_description)).setText(
+				cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_DESCRIPTION)));
 
-        ((TextView) view.findViewById(R.id.event_row_description)).setText(
-                cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_DESCRIPTION)));
+		((TextView) view.findViewById(R.id.event_row_date)).setText(
+				cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_DATE)));
 
-        ((TextView) view.findViewById(R.id.event_row_date)).setText(
-                cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_DATE)));
+		((TextView) view.findViewById(R.id.event_row_time)).setText(
+				cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_TIME)));
 
-        ((TextView) view.findViewById(R.id.event_row_time)).setText(
-                cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_TIME)));
+		String location = "Lat: " + cursor.getFloat(cursor.getColumnIndexOrThrow(EventsTable.COL_LAT)) + ", "
+				+ "lng: " + cursor.getFloat(cursor.getColumnIndexOrThrow(EventsTable.COL_LNG));
 
-        String location = "Lat: " + cursor.getFloat(cursor.getColumnIndexOrThrow(EventsTable.COL_LAT)) + ", "
-                + "lng: " + cursor.getFloat(cursor.getColumnIndexOrThrow(EventsTable.COL_LNG));
+		((TextView) view.findViewById(R.id.event_row_location)).setText(location);
 
-        ((TextView) view.findViewById(R.id.event_row_location)).setText(location);
-
-    }
+	}
 }
