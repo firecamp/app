c063dd8540899987afe5a0b64fd8c7544a2c1290
FS <example@example.com>
2014-03-21 16:56:05 -0400

    Added image caching

--- src/ca/xef6/app/util/EventAdapter.java
+++ src/ca/xef6/app/util/EventAdapter.java
@@ -1,9 +1,14 @@
 package ca.xef6.app.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.widget.ResourceCursorAdapter;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,38 +17,48 @@ import ca.xef6.app.db.EventsTable;
 
 public class EventAdapter extends ResourceCursorAdapter {
 
-	public EventAdapter(Context context, int layout, Cursor c, int flags) {
-		super(context, layout, c, flags);
+    private final Map<String, Bitmap> bitmapCache = new HashMap<String, Bitmap>();
+
+    public EventAdapter(Context context, int layout, Cursor c, int flags) {
+	super(context, layout, c, flags);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+	String imageUrl = cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_IMAGEURL));
+
+	Log.w("EventAdapter", "imageUrl = " + imageUrl);
+	ImageView iv = ((ImageView) view.findViewById(R.id.event_row_image));
+	if (imageUrl != null) {
+	    if (!bitmapCache.containsKey(imageUrl)) {
+		bitmapCache.put(imageUrl, BitmapFactory.decodeFile(imageUrl));
+	    }
+	    iv.setImageBitmap(bitmapCache.get(imageUrl));
+	} else {
+	    iv.setVisibility(View.GONE);
 	}
 
-	@Override
-	public void bindView(View view, Context context, Cursor cursor) {
+	((TextView) view.findViewById(R.id.event_row_name)).setText(
+		cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_NAME)));
 
-		String imageUrl = cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_IMAGEURL));
-		//Log.w("EventAdapter", "imageUrl = " + imageUrl);
-		ImageView iv = ((ImageView) view.findViewById(R.id.event_row_image));
-		if (imageUrl != null) {
-			iv.setImageBitmap(BitmapFactory.decodeFile(imageUrl));
-		} else {
-			iv.setVisibility(View.GONE);
-		}
+	((TextView) view.findViewById(R.id.event_row_description)).setText(
+		cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_DESCRIPTION)));
 
-		((TextView) view.findViewById(R.id.event_row_name)).setText(
-				cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_NAME)));
+	((TextView) view.findViewById(R.id.event_row_date)).setText(
+		cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_DATE)));
 
-		((TextView) view.findViewById(R.id.event_row_description)).setText(
-				cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_DESCRIPTION)));
+	((TextView) view.findViewById(R.id.event_row_time)).setText(
+		cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_TIME)));
 
-		((TextView) view.findViewById(R.id.event_row_date)).setText(
-				cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_DATE)));
+	TextView locationTV = ((TextView) view.findViewById(R.id.event_row_location));
 
-		((TextView) view.findViewById(R.id.event_row_time)).setText(
-				cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_TIME)));
-
-		String location = "Lat: " + cursor.getFloat(cursor.getColumnIndexOrThrow(EventsTable.COL_LAT)) + ", "
-				+ "lng: " + cursor.getFloat(cursor.getColumnIndexOrThrow(EventsTable.COL_LNG));
-
-		((TextView) view.findViewById(R.id.event_row_location)).setText(location);
-
+	String location = cursor.getString(cursor.getColumnIndexOrThrow(EventsTable.COL_LOC_ADDRESS));
+	if (location == null) {
+	    location = "Lat: " + cursor.getFloat(cursor.getColumnIndexOrThrow(EventsTable.COL_LAT)) + ", "
+		    + "lng: " + cursor.getFloat(cursor.getColumnIndexOrThrow(EventsTable.COL_LNG));
 	}
+	locationTV.setText(location);
+
+    }
 }
